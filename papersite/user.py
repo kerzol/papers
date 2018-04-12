60969dc7a29936b24414df7dc054f723823050b4
Sergey Kirgizov <kerzolster@gmail.com>
2018-04-12 22:45:03 +0200

    super admin fix

--- papersite/user.py
+++ papersite/user.py
@@ -32,7 +32,7 @@ def get_user_id():
         return ANONYMOUS
 
 def is_super_admin(userid):
-    return query_db("select super_admin  \
+    return 1 == query_db("select super_admin  \
                           from users          \
                           where userid = ?", [userid], one=True)['super_admin']
 
