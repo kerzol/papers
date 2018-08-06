a9e98e23b43cf514f48920142f328241af461a17
Sergey Kirgizov <kerzolster@gmail.com>
2018-08-06 23:43:34 +0200

    Add emails suffix to spam list

--- papersite/spamdetector.py
+++ papersite/spamdetector.py
@@ -1,8 +1,13 @@
 ### Stupid, but extensible spam detector
 
-STOP_SPAM_WORDS = ["Loan", "Lender"]
+STOP_SPAM_WORDS = ["Loan", "Lender", ".trade", ".bid", ".men"]
 
 def is_spam(request):
+  if 'email' in request.form:
+    if any(
+        [ word.upper() in request.form['email'].upper()
+         for word in STOP_SPAM_WORDS]):
+      return True
   if 'username' in request.form:
     if any(
         [ word.upper() in request.form['username'].upper()
